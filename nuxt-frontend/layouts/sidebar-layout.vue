c87c8e9929b0d7727819c5eec2285831af19abcb
lvllvl <chrisidxloop@gmail.com>
2023-09-26 21:44:41 -0400

    update basic stuff on frontend

---
+++ nuxt-frontend/layouts/sidebar-layout.vue
@@ -0,0 +1,48 @@
+<!-- layouts/sidebar-layout.vue -->
+<template>
+  <div class="sidebar-layout">
+    <aside class="sidebar">
+      <!-- Sidebar content goes here -->
+      <nav>
+        <slot name="sidebarContent"></slot>
+      </nav>
+    </aside>
+    <main class="main-content">
+      <!-- Main content goes here -->
+      <h1>{{ pageTitle }}</h1>
+      <slot></slot> <!-- Default slot for page content -->
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageTitle: String,
+  },
+}
+</script>
+
+<style scoped>
+/* Add your CSS styles for the layout here */
+.sidebar-layout {
+  display: flex;
+  flex-direction: row;
+  background-color: #f0f0f0;
+  padding: 20px;
+}
+
+.sidebar {
+  /* Sidebar styles */
+  width: 25%;
+  background-color: #ffffff;
+  padding: 10px;
+  color: #333; /* Set the font color to dark gray (#333) */
+}
+
+.main-content {
+  /* Main content styles */
+  width: 75%;
+  padding: 20px;
+}
+</style>
--- nuxt-frontend/pages/index.vue
+++ nuxt-frontend/pages/index.vue
@@ -1,10 +1,26 @@
-<template lang="pug">
-  tutorial
+<!-- pages/index.vue -->
+<template>
+  <SidebarLayout pageTitle="Parallel Image Processing">
+    <!-- Sidebar content goes here -->
+    <template v-slot:sidebarContent>
+      <div>
+        <p style="color: red;">Here is some text, it's 816 a person</p>
+      </div>
+    </template>
+    
+    <!-- Page content goes here -->
+    <div>
+      <!-- Your page content -->
+    </div>
+  </SidebarLayout>
 </template>
 
 <script>
-import Vue from 'vue'
+import SidebarLayout from '~/layouts/sidebar-layout.vue';
 
-export default Vue.extend({
-  name: 'IndexPage'
-})
+export default {
+  components: {
+    SidebarLayout,
+  },
+}
+</script>
