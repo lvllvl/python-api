f644772e88b8ed6049e6ea2ca7a8b1320347d0c8
lvllvl <chrisidxloop@gmail.com>
2023-09-28 18:37:32 -0400

    update color-to-grayscale function

--- cuda/colorToGrayscale.cu
+++ cuda/colorToGrayscale.cu
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <cuda_runtime.h>
 
-__global__
-void colortoGrayscaleConvertion( 
+#define CHANNELS 3
+
+__global__ void colortoGrayscaleConvertion(
     unsigned char * Pout,
     unsigned char * Pin,
-    int width, 
-    int height ) {
+    int width,
+    int height)
+{
 
-        int col = blockIdx.x *blockDim.x + threadIdx.x;
-        int row = blockIdx.y *blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (col < width && row < height)
+    {
 
         // Get ID offset for the grayscale image
         int grayOffset = row * width + col;
@@ -17,11 +23,12 @@ void colortoGrayscaleConvertion(
         // times more columns than the gray scale image
 
         int rgbOffset = grayOffset * CHANNELS;
-        unsigned char r = Pin[ rgbOffset ]; // Red value
-        unsigned char g = Pin[ rgbOffset + 2 ]; // Green value
-        unsigned char b = Pin[ rgbOffset + 3 ]; // Blue value
+        unsigned char r = Pin[rgbOffset];     // Red value
+        unsigned char g = Pin[rgbOffset + 1]; // Green value
+        unsigned char b = Pin[rgbOffset + 2]; // Blue value
 
-        // Perform the rescaling and store it 
+        // Perform the rescaling and store it
         // We multiply by floating point constants
-        Pout[ grayOffset ] = 0.21f*r + 0.71f*g + 0.07f*b;
+        Pout[grayOffset] = static_cast<unsigned char>(round(0.21f * r + 0.71f * g + 0.07f * b));
     }
+}
