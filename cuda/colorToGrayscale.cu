f21e67366fe33428b7c29eec28cd0fe05e89a87e
lvllvl <chrisidxloop@gmail.com>
2023-09-28 18:23:45 -0400

    add color-to-grayscale function

---
+++ cuda/colorToGrayscale.cu
@@ -0,0 +1,27 @@
+#include <iostream>
+
+__global__
+void colortoGrayscaleConvertion( 
+    unsigned char * Pout,
+    unsigned char * Pin,
+    int width, 
+    int height ) {
+
+        int col = blockIdx.x *blockDim.x + threadIdx.x;
+        int row = blockIdx.y *blockDim.y + threadIdx.y;
+
+        // Get ID offset for the grayscale image
+        int grayOffset = row * width + col;
+
+        // One can think of the RGB image having CHANNEL
+        // times more columns than the gray scale image
+
+        int rgbOffset = grayOffset * CHANNELS;
+        unsigned char r = Pin[ rgbOffset ]; // Red value
+        unsigned char g = Pin[ rgbOffset + 2 ]; // Green value
+        unsigned char b = Pin[ rgbOffset + 3 ]; // Blue value
+
+        // Perform the rescaling and store it 
+        // We multiply by floating point constants
+        Pout[ grayOffset ] = 0.21f*r + 0.71f*g + 0.07f*b;
+    }
