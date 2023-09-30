7528ece4ba69adfac90f11c64c5f8c030720b9ed
lvllvl <chrisidxloop@gmail.com>
2023-09-30 15:23:11 -0400

    update on the frontend

--- nuxt-frontend/pages/index.vue
+++ nuxt-frontend/pages/index.vue
@@ -1,26 +1,54 @@
-<!-- pages/index.vue -->
 <template>
-  <SidebarLayout pageTitle="Parallel Image Processing">
-    <!-- Sidebar content goes here -->
-    <template v-slot:sidebarContent>
-      <div>
-        <p style="color: red;">Here is some text, it's 816 a person</p>
-      </div>
-    </template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
     
-    <!-- Page content goes here -->
-    <div>
-      <!-- Your page content -->
+    <!-- Title -->
+    <h1 class="text-4xl font-bold mb-8">Parallel Image Processing</h1>
+    
+    <!-- Images -->
+    <div class="flex">
+      <!-- Preloaded Image -->
+      <img src="../../data/image_portrait.png" alt="Preloaded Image" class="mr-4 w-64 h-64">
+
+      
+      <!-- Processed Image -->
+      <img v-if="processedImage" :src="processedImage" alt="Processed Image">
     </div>
-  </SidebarLayout>
+    
+    <!-- Buttons -->
+    <div class="mt-8">
+      <p class="mb-4 text-lg font-semibold">Parallel image processing options:</p>
+      <button @click="processImage('color-to-grayscale')" class="bg-blue-500 text-white py-2 px-4 rounded mr-4 hover:bg-blue-600">
+        Color-to-grayscale
+      </button>
+      <button @click="processImage('image-blur')" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+        Image Blur
+      </button>
+      <button @click="processImage('reset-image')" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+        Reset 
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import SidebarLayout from '~/layouts/sidebar-layout.vue';
-
 export default {
-  components: {
-    SidebarLayout,
+  data() {
+    return {
+      processedImage: null
+    }
   },
+  methods: {
+    async processImage(option) {
+      // Call the Flask API with the selected option
+      // Update the `processedImage` data property with the returned image path
+    }
+  }
 }
 </script>
+
+<style scoped>
+img {
+    width: 500px;
+    height: 400px;
+}
+</style>
