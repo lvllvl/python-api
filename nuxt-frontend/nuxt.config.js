f4f04a04cbd386867f62ff6ec10e9568810c64b1
lvllvl <chrisidxloop@gmail.com>
2023-10-04 21:26:52 -0400

    Added a dist folder, npm run generate

--- nuxt-frontend/nuxt.config.js
+++ nuxt-frontend/nuxt.config.js
@@ -15,6 +15,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  generate: {
+    dir: 'dist'
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
